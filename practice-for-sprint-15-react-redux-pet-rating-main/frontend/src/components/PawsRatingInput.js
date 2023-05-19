aa494b8996844bc5b3606816cd3121c54728d041
Jiamin-Zou <jiaminz622@gmail.com>
2023-05-19 13:47:16 -0400

    Completed pet rating bonus

--- practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
@@ -1,11 +1,9 @@
 import { useState, useEffect } from "react";
 
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
+  const [activeRating, setActiveRating] = useState(rating);
 
-  const [activeRating, setActiveRating] = useState(rating)
-
-  useEffect( () => setActiveRating(rating)
-  , [rating])
+  useEffect(() => setActiveRating(rating), [rating]);
 
   return (
     <>
@@ -15,34 +13,60 @@ const PawsRatingInput = ({ rating, disabled, onChange }) => {
         value={rating}
         onChange={onChange}
       />
-      <div className="rating-input" onMouseLeave={() => setActiveRating(rating)}>
-        <div className={activeRating >= 1 ? "filled" : "empty"}
-          onMouseEnter={() => disabled ? null : setActiveRating(1)}
-          onClick = {() => onChange(1)}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div className={activeRating >= 2 ? "filled" : "empty"}
-          onMouseEnter={() => disabled ? null : setActiveRating(2)}
-          onClick = {() => onChange(2)}
+      <div
+        className="rating-input"
+        onMouseLeave={() => setActiveRating(rating)}
+      >
+        {[1, 2, 3, 4, 5].map((i) => (
+          <div
+            className={activeRating >= i ? "filled" : "empty"}
+            onMouseEnter={() => (disabled ? null : setActiveRating(i))}
+            onClick={() => onChange(i)}
           >
-          <i className="fa fa-paw"></i>
-        </div>
-        <div className={activeRating >= 3 ? "filled" : "empty"}
-          onMouseEnter={() => disabled ? null : setActiveRating(3)}
-          onClick = {() => onChange(3)}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div className={activeRating >= 4 ? "filled" : "empty"}
-          onMouseEnter={() => disabled ? null : setActiveRating(4)}
-          onClick = {() => onChange(4)}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div className={activeRating >= 5 ? "filled" : "empty"}
-          onMouseEnter={() => disabled ? null : setActiveRating(5)}
-          onClick = {() => onChange(5)}>
-          <i className="fa fa-paw"></i>
-        </div>
+            <i className="fa fa-paw"></i>
+          </div>
+        ))}
       </div>
+      {/* <div
+        className="rating-input"
+        onMouseLeave={() => setActiveRating(rating)}
+      >
+        <div
+          className={activeRating >= 1 ? "filled" : "empty"}
+          onMouseEnter={() => (disabled ? null : setActiveRating(1))}
+          onClick={() => onChange(1)}
+        >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div
+          className={activeRating >= 2 ? "filled" : "empty"}
+          onMouseEnter={() => (disabled ? null : setActiveRating(2))}
+          onClick={() => onChange(2)}
+        >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div
+          className={activeRating >= 3 ? "filled" : "empty"}
+          onMouseEnter={() => (disabled ? null : setActiveRating(3))}
+          onClick={() => onChange(3)}
+        >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div
+          className={activeRating >= 4 ? "filled" : "empty"}
+          onMouseEnter={() => (disabled ? null : setActiveRating(4))}
+          onClick={() => onChange(4)}
+        >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div
+          className={activeRating >= 5 ? "filled" : "empty"}
+          onMouseEnter={() => (disabled ? null : setActiveRating(5))}
+          onClick={() => onChange(5)}
+        >
+          <i className="fa fa-paw"></i>
+        </div>
+      </div> */}
     </>
   );
 };
